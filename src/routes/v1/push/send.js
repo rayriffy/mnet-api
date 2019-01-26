4f5568d5d39c1af65bf6cc957633e1495d7039b5
Phumrapee Limpianchop <git@rayriffy.com>
2019-01-26 22:09:55 +0700

    :sparkles: Push Notification

--- src/routes/v1/push/send.js
+++ src/routes/v1/push/send.js
@@ -1,13 +1,34 @@
+import axios from 'axios'
+import dotenv from 'dotenv'
 import express from 'express'
+
+dotenv.config()
+const {FCM_KEY} = process.env
 
 const router = express.Router()
 
 router.post('/', (req, res) => {
-  let data = req.body
-  console.log(data)
+  const payload = {
+    to: `/topics/${req.body.to}`,
+    priority: 'normal',
+    notification: {
+      title: req.body.title,
+      text: req.body.text,
+    },
+  }
+
+  const options = {
+    headers: {
+      Authorization: `key=${FCM_KEY}`,
+      'Content-Type': 'application/json',
+    },
+  }
+
+  axios.post('https://fcm.googleapis.com/fcm/send', payload, options)
+
   res.status(200).send({
     status: 'success',
-    response: 'this api will use to send notification to devices',
+    response: `sending push notification to ${req.body.to}`,
   })
 })
 
