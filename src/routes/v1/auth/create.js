64c56db31884ada2b4536e2f6ce71cf4e0928db4
Phumrapee Limpianchop <git@rayriffy.com>
2019-01-27 10:39:08 +0700

    :lock: Only authenticated admin will be able to create other users

--- src/routes/v1/auth/create.js
+++ src/routes/v1/auth/create.js
@@ -1,8 +1,33 @@
 import express from 'express'
+import passport from 'passport'
 
 import User from '../../../models/user'
 
 const router = express.Router()
+
+router.all('*', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+  User.getUserById(req.user.id, (err, user) => {
+    if (err) {
+      res.status(401).send({
+        status: 'failure',
+        response: {
+          message: 'id not found',
+        },
+      })
+    } else {
+      if (user.role !== 'administrator') {
+        res.status(401).send({
+          status: 'failure',
+          response: {
+            message: 'insufficient permission',
+          },
+        })
+      } else {
+        next()
+      }
+    }
+  })
+})
 
 router.post('/', (req, res) => {
   const userData = new User({
