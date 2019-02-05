69c087215f553289fa7cf65e2d55cecbd79b49e2
Phumrapee Limpianchop <git@rayriffy.com>
2019-02-05 17:03:49 +0700

    :sparkles: Functional announce create request

--- src/routes/v1/announce/create.js
+++ src/routes/v1/announce/create.js
@@ -1,9 +1,62 @@
 import express from 'express'
 
+import Announce from '../../../models/announce'
+import User from '../../../models/user'
+
 const router = express.Router()
 
+router.post('/', (req, res, next) => {
+  User.getUserById(req.user.id, (err, user) => {
+    if (err) {
+      res.status(404).send({
+        status: 'failure',
+        response: {
+          message: 'id not found',
+        },
+      })
+    } else {
+      if (user.role !== 'administrator') {
+        res.status(401).send({
+          status: 'failure',
+          response: {
+            message: 'insufficient permission',
+          },
+        })
+      } else {
+        next()
+      }
+    }
+  })
+})
+
 router.post('/', (req, res) => {
-  
+  const payload = {
+    message: req.body.message,
+  }
+
+  Announce.addAnnounce(payload, (err, announce) => {
+    if (err) {
+      return res.status(400).send({
+        status: 'failure',
+        response: {
+          message: 'failed to create new group',
+          data: err,
+        },
+      })
+    } else {
+      return res.status(200).send({
+        status: 'success',
+        response: {
+          message: 'announce created',
+          data: {
+            group: {
+              id: announce._id,
+            },
+          },
+        },
+      })
+    }
+  })
 })
 
 router.all('/', (req, res) => {
